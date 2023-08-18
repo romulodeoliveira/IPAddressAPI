d92e1d942680cff18c23202caefec7214d6b5aa0
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-17 22:52:43 -0300

    modificação da obtenção do ipv6 e adição da data

--- Controllers/IPAddressController.cs
+++ Controllers/IPAddressController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Net.Sockets;
 
 namespace IPAddressAPI.Controllers
 {
@@ -13,11 +16,20 @@ namespace IPAddressAPI.Controllers
 
             var ipv4 = remoteIpAddress.MapToIPv4();
             var ipv6 = remoteIpAddress.MapToIPv6();
+            
+            if (remoteIpAddress.AddressFamily == AddressFamily.InterNetworkV6 &&
+                ipv6.IsIPv4MappedToIPv6)
+            {
+                ipv6 = ipv6.MapToIPv4();
+            }
+
+            var accessDate = DateTime.Now;
 
             return Ok(new
             {
                 IPv4 = ipv4.ToString(),
-                IPv6 = ipv6.ToString()
+                IPv6 = ipv6.ToString(),
+                AccessDate = accessDate.ToString()
             });
         }
     }
