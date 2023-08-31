6c19aa5d3762056475b6682dd70d7125ea2ac499
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-31 00:37:57 -0300

    personalização do swagger

--- Program.cs
+++ Program.cs
@@ -1,8 +1,30 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "1.0",
+        Title = "Send Email API",
+        Description = "Projeto destinado a portfólio. Por favor, tenha consciência ao utilizá-lo.",
+        Contact = new OpenApiContact
+        {
+            Name = "Romulo de Oliveira",
+            Email = "devromulodeoliveira@gmail.com",
+            Url = new Uri("https://github.com/romulodeoliveira"),
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Licença",
+            Url = new Uri("https://github.com/romulodeoliveira/IPAddressAPI/blob/main/LICENSE.md"),
+        }
+    });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
